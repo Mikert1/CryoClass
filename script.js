3dff82969e6acc4b16bf259af199e8f07b28ea3d
Mike Balvert <mikebalvert@icloud.com>
2024-12-26 22:16:01 +0100

    more names added

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-shortNames = {
+const shortNames = {
     d: { name : 'display', value : { n: 'none', b: 'block', i: 'inline', ib: 'inline-block', f: 'flex', g: 'grid', } },
-    jc: { name : 'justifyContent', value : { s: 'space-between', c: 'center', e: 'end', b: 'space-between', a: 'space-around', } },
+    jc: { name : 'justifyContent', value : { s: 'space-between', c: 'center', e: 'end', b: 'space-between', a: 'space-around', sb: 'space-between', sa: 'space-around'} },
     ai: { name : 'alignItems', value : { s: 'stretch', c: 'center', e: 'end', b: 'baseline', } },
     fd: { name : 'flexDirection', value : { r: 'row', rr: 'row-reverse', c: 'column', cr: 'column-reverse', } },
     fw: { name : 'flexWrap', value : { n: 'nowrap', w: 'wrap', wr: 'wrap-reverse', } },
-    ac: { name : 'alignContent', value : { s: 'stretch', c: 'center', e: 'end', b: 'space-between', a: 'space-around', } },
+    ac: { name : 'alignContent', value : { s: 'stretch', c: 'center', e: 'end', b: 'space-between', a: 'space-around', sb: 'space-between', sa: 'space-around'} },
     as: { name : 'alignSelf', value : { a: 'auto', s: 'stretch', c: 'center', e: 'end', b: 'baseline', } },
+    p: { name : 'position', value : { s: 'static', r: 'relative', a: 'absolute', f: 'fixed', i: 'sticky' } },
 
     m: { name : 'margin', value : { a: 'auto', } },
     p: { name : 'padding', value : { a: 'auto', } },
@@ -14,9 +15,25 @@ shortNames = {
     f: { name : 'flex', value : { a: 'auto', } },
     o: { name : 'order', value : { a: 'auto', } },
     g: { name : 'grid', value : { a: 'auto', } },
+    z: { name : 'zIndex', value : { a: 'auto', } },
 
-    c: { name : 'color', value : { r: 'red', g: 'green', b: 'blue', y: 'yellow', p: 'purple', o: 'orange', b: 'black', w: 'white', g: 'gray', b: 'brown', } },
-    bc: { name : 'backgroundColor', value : { r: 'red', g: 'green', b: 'blue', y: 'yellow', p: 'purple', o: 'orange', b: 'black', w: 'white', g: 'gray', b: 'brown', } },
+    c: { name : 'color', value : { r: 'red', g: 'green', b: 'blue', y: 'yellow', o: 'orange', w: 'white', g: 'gray', } },
+    bc: { name : 'backgroundColor', value : { r: 'red', g: 'green', b: 'blue', y: 'yellow', o: 'orange', w: 'white', g: 'gray', } },
+    b: { name : 'border', value : { n: 'none', } },
+    br: { name : 'borderRadius', value : { n: 'none', } },
+    bs: { name : 'boxShadow', value : { n: 'none', } },
+
+    font: { name : 'fontSize', value : { s: 'small', m: 'medium', l: 'large', xl: 'x-large', xxl: 'xx-large', } },
+    fontw: { name : 'fontWeight', value : { l: 'lighter', n: 'normal', b: 'bold', } },
+    fonts: { name : 'fontStyle', value : { n: 'normal', i: 'italic', o: 'oblique', } },
+    fontv: { name : 'fontVariant', value : { n: 'normal', s: 'small-caps', } },
+    fontf: { name : 'fontFamily', value : { s: 'serif', ss: 'sans-serif', m: 'monospace', c: 'cursive', f: 'fantasy', } },
+    td: { name : 'textDecoration', value : { n: 'none', u: 'underline', o: 'overline', l: 'line-through', b: 'blink', } },
+    ir : { name : 'imageRendering', value : { c: 'crisp-edges', p: 'pixelated', o: 'optimizeQuality', a: 'auto' } },
+    ta: { name : 'textAlign', value : { l: 'left', r: 'right', c: 'center', j: 'justify', } },
+
+    cursor: { name : 'cursor', value : { a: 'auto', d: 'default', n: 'none', p: 'pointer', m: 'move', t: 'text', h: 'help', } },
+
 };
 
 function shortNameToFullName(name) {
